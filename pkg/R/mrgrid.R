1bda526c1e0fe21256d4fce1d977b8f6814c0f9e
sigurdur (Mr ) <sigurdur@hafro.is>
2011-10-25 17:10:01 +0000

    New mrgrid function, with help, version increment, change in authors list
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geoextras@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mrgrid.R
+++ pkg/R/mrgrid.R
@@ -1,20 +1,33 @@
 mrgrid <-
-function(mr, dlat = 5, dlon = 10, fill = F, ...)
+function (mr, dlat = 5, dlon = 10, fill = F, col = "black", ...) 
 {
-	n <- length(mr)
-	lat <- mr2d(mr, dlat = dlat, dlon = dlon)$lat
-	lon <- mr2d(mr, dlat = dlat, dlon = dlon)$lon
-	lat <- c(rep(lat, 5), rep(NA, n))
-	lon <- c(rep(lon, 5), rep(NA, n))
-	lat <- as.vector(matrix(matrix(lat, nrow = 6, byrow = T), ncol = 1))
-	lon <- as.vector(matrix(matrix(lon, nrow = 6, byrow = T), ncol = 1))
-	lat <- lat + c(dlat/120, dlat/120,  - dlat/120,  - dlat/120, dlat/
-		120, NA)
-	lon <- lon + c(dlon/120,  - dlon/120,  - dlon/120, dlon/120, dlon/
-		120, NA)
-        lat <- data.frame(lat = lat, lon = lon)
-	if(fill)
-		geopolygon(lat, ...)
-	else geolines(lat, ...)
+  mr <- lapply(mr, mrPeri, dlat = dlat, dlon = dlon)
+  if(fill) {
+    if(length(col) == 1) {
+      invisible(lapply(mr, geopolygon, col = col, ...))
+    }
+    else {
+      if(length(mr) != length(col)) {
+        stop("More than one color but not as many as the rectangles")
+      }
+      else {
+        invisible(lapply(1:length(mr), function(i, mr, col)
+          geopolygon(mr[[i]], col = col[i], ...), mr = mr, col = col, ...))
+      }
+    }
+  }
+  else {
+    if(length(col) == 1) {
+      invisible(lapply(mr, geolines,  col = col, ...))
+    }
+    else {
+      if(length(mr) != length(col)) {
+        stop("More than one color but not as many as the rectangles")
+      }
+      else {
+        invisible(lapply(1:length(mr), function(i, mr, col, ...)
+          geolines(mr[[i]], col = col[i], ...), mr = mr, col = col, ...))
+      }
+    }
+  }
 }
-
